3af089e1a090ada454d9f021711b029599ce128a
wonhong1994 <wonhong1994@naver.com>
2024-06-14 16:16:59 +0900

    first commit

---
+++ src/main/java/com/exam/mapper/MemberMapper.java
@@ -0,0 +1,14 @@
+package com.exam.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.exam.dto.Member;
+
+
+@Mapper
+public interface MemberMapper {
+
+	public int save(Member mem);
+	public Member findById(String userid);
+}
